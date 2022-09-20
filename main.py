273e4d88faff8f939416ffb015466a78ae127841
N SANTOSH <34029613+santosh8309@users.noreply.github.com>
2022-09-20 21:08:49 +0530

    cloud build yml added

--- main.py
+++ main.py
@@ -28,5 +28,5 @@ def hello():
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
 # [END gae_flex_quickstart]
